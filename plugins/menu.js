1a6902a7584839c8530778c01a75148afbaddc01
Bebi-n <Gihangagana77@gmail.com>
2025-06-05 09:50:50 +0530

    Update and rename listcmd.js to menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@ const config = require('../config')
 const { cmd, commands } = require('../command');
 
 cmd({
-    pattern: "list",
+    pattern: "menu",
     alias: ["listcmd","commands"],
     desc: "menu the bot",
     category: "menu",
